59e65385e120521aeba832a3e49f3bc6f0f7e523
Jaime Alberto Rocha <jaimerocha2006@gmail.com>
2024-01-17 21:08:52 -0500

    logica completada

--- src/service/CalculadoraService.java
+++ src/service/CalculadoraService.java
@@ -9,5 +9,13 @@ public class CalculadoraService {
 	public int restar(int a, int b) {
 		return a - b;
 	}
+	
+	public int multiplicar(int a, int b) {
+		return a * b;
+	}
+	
+	public int dividir(int a, int b) {
+		return a / b;
+	}
 
 }
