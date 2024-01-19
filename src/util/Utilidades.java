a5532159e70738e98dea77a87db97f28eb2dd914
Jaime Alberto Rocha <jaimerocha2006@gmail.com>
2024-01-19 00:42:52 -0500

    añadimos nueva clase utilidades en development

---
+++ src/util/Utilidades.java
@@ -0,0 +1,13 @@
+package util;
+
+public final class Utilidades {
+
+	public static int mayor(int x, int y) {
+		return Math.max(x, y);
+	}
+	
+	public static int min(int x, int y) {
+		return Math.min(x, y);
+	}
+	
+}
